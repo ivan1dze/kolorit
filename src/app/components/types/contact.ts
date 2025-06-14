7c96b8c8d06392bd6626d92f92f212932df3ff35
quickjeembo <vanya9782@gmail.com>
2025-06-14 20:15:02 +0300

    feat: types for contacts

---
+++ src/app/components/types/contact.ts
@@ -0,0 +1,10 @@
+export type Contact = {
+    id: number;
+    phone: string;
+    email?: string;
+    instagram?: string;
+    telegram?: string;
+    whatsapp?: string;
+    viber?: string;
+    vk?: string;
+};
