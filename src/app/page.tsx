f88f3e32ca1af82f9af47413b3189914bec1c75c
quickjeembo <vanya9782@gmail.com>
2025-06-15 00:35:27 +0300

    feat: add ContactCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import PromoBlock from "@/app/components/main/PromoBlock";
 import styles from "./page.module.css"
+import ContactCard from "@/app/components/ContactCard/ContactCard";
 
 export default function Home() {
     return (
         <main>
-                <PromoBlock />
+            <PromoBlock/>
+            <ContactCard/>
         </main>
     );
 }
