011293931a495f968daa9178dcda99b58ee07bb1
quickjeembo <vanya9782@gmail.com>
2025-06-16 01:25:54 +0300

    feat: contact page / styles

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,23 @@
+import ContactCard from "../components/ContactCard/ContactCard";
+import Image from 'next/image';
+import styles from './ContactsPage.module.css';
+
+export default function ContactsPage() {
+    return (
+        <main className={styles.page}>
+            <div className={styles.cardWrapper}>
+                <ContactCard />
+            </div>
+
+            <div className={styles.imageWrapper}>
+                <Image
+                    src="/images/kolorit-storefront.png" // сохрани файл сюда: public/images/kolorit-storefront.png
+                    alt="Магазин Колорит"
+                    width={1216}
+                    height={381}
+                    className={styles.image}
+                />
+            </div>
+        </main>
+    );
+}
